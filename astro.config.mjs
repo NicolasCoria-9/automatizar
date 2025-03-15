aafb1f1ef0f6fe12de1bed1e3261c28e8cb83ce7
Nicolas_Coria <nicolasacoria@gmail.com>
2025-03-15 15:56:48 -0300

    Configurando Astro para SSR con adaptador de Node.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   site: 'https://nicolascoria-9.github.io',
   base: '/automatizar'
 });
