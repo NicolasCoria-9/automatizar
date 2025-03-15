046fc23d35015dfb761e82c0471a8005f497df57
Nicolas_Coria <nicolasacoria@gmail.com>
2025-03-15 16:28:21 -0300

    Corrigiendo configuración de Astro para build estático

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
-  site: 'https://NicolasCoria-9.github.io',
-  base: 'automatizar'
+  output: 'static',
+  site: 'https://nicolascoria-9.github.io',
+  base: '/automatizar'
 });
