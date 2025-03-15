2307c5452478a20f285dd35787acf6f8ea08f9f0
Nicolas_Coria <nicolasacoria@gmail.com>
2025-03-15 16:01:08 -0300

    Cambiando a Web3Forms para el formulario de contacto y volviendo a build estático

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
   site: 'https://nicolascoria-9.github.io',
   base: '/automatizar'
 });
